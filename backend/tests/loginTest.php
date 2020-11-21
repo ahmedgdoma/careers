2012a1ddb4e27a3c127d4924c77ef341d8161b0a
Ahmed Doma <ahmedgdoma@gmail.com>
2020-11-21 02:41:35 +0200

    fix test

--- backend/tests/loginTest.php
+++ backend/tests/loginTest.php
@@ -8,14 +8,32 @@ class loginTest extends TestCase
 
     public function testFailLogin()
     {
-        $response = $this->call('POST', '/login', ['email' => 'admin@careers.dev', 'password'=> 'client']);
+        $response = $this->call('POST', '/admin/login', ['email' => 'admin@careers.dev', 'password'=> 'client']);
         $this->assertEquals(401, $response->status());
 
     }
     public function testSuccessLogin()
     {
-        $response = $this->call('POST', '/login', ['email' => 'admin@careers.dev', 'password'=> 'admin']);
+        $response = $this->call('POST', '/admin/login', ['email' => 'admin@careers.dev', 'password'=> 'admin']);
         $this->assertEquals(200, $response->status());
+        return $response['access_token'];
+
+    }
+
+    public function testFailLogout()
+    {
+        $this->json('POST', '/admin/logout');
+        $this->assertEquals(401, $this->response->status());
+
+    }
+
+    /**
+     * @depends testSuccessLogin
+     */
+    public function testSuccessLogout($token)
+    {
+        $this->json('POST', '/admin/logout', [], ['Authorization' => 'bearer ' . $token]);
+        $this->assertEquals(200, $this->response->status());
 
     }
 }
